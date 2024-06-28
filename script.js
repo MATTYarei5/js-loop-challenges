c9f727ee5f9d67d858a07f1b3ff7e506c7d6e419
Matt Nebeker <mattn@devpipeline.com>
2024-06-28 00:05:22 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+for (let i = 0; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+const arr = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const val = [4, 6];
+let newArr = [];
+
+for (let i = 0; i < val.length; i++) {
+  let results = val[i];
+  if (results >= 0 && results < arr.length) {
+    newArr.push(arr[results]);
+  }
+}
+console.log(newArr);
+
+const numToArr = 12345;
+let newArrResults = [];
+
+for (let num of String(numToArr)) {
+  newArrResults.push(num);
+}
+console.log(newArrResults);
+
+const arrayOne = [1, 2, 3];
+const arrayTwo = ["a", "b", "c", "d"];
+const comboArr = [];
+
+for (let num = 0; num < arrayOne.length; num++) {
+  for (let letter = 0; letter < arrayTwo.length; letter++) {
+    comboArr.push([arrayOne[num], arrayTwo[letter]]);
+    comboArr.push([arrayTwo[letter], arrayOne[num]]);
+  }
+}
+console.log(comboArr);
